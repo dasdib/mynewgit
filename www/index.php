420bcdc3a8426fd53d36b414bd6d6b3a7161ecb6
Dibyendu Das <ddas446@its.jnj.com>
2020-06-07 02:18:55 +0530

    New Sonar #50

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ $runInstance =  new ScoreBoardStats();
 $runInstance->setTotalRuns(100);
 
 // Virat scores 50
-$runInstance->scoreRuns(80);  
+$runInstance->scoreRuns(82);  
 
 echo "Runs Remaining ".$runInstance->getRemainingRuns();
 ?>
