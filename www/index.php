9ceead4ef0d9ae7280d915c4b98e2ec2043a1be1
Dibyendu Das <ddas446@its.jnj.com>
2020-06-07 04:51:14 +0530

    New Test#3

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ $runInstance =  new ScoreBoardStats();
 $runInstance->setTotalRuns(100);
 
 // Virat scores 50
-$runInstance->scoreRuns(86);  
+$runInstance->scoreRuns(89);  
 
 echo "Runs Remaining ".$runInstance->getRemainingRuns();
 ?>
